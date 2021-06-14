5117f99078a6f5271f1759c1cd9f82ab9f287630
Daniel Mantovani <dmanto@gmail.com>
2021-06-14 13:13:15 -0300

    added addCode function

--- src/main.js
+++ src/main.js
@@ -1,4 +1,8 @@
 import chalk from 'chalk';
-import {binLog} from '../lib/binary-collections.js';
+import {binLog, addCode} from '../lib/binary-collections.js';
 
 console.log(chalk.green(binLog('000000000000100000000000000000000000000000000')));
+
+// console.log(chalk.red(addCode('10101010101010101010101010101010101010101010101010101010101010101010101010101010', 1, {})));
+// console.log(chalk.red(JSON.stringify(addCode('1010101010101010101010101010101010101010', 1, {}))));
+console.log(chalk.red(addCode('1010101010101010101010101010101010101010', 1, {})));
